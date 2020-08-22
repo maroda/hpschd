0fd56c0f315e2ffbaa5493505362b8561195fa9b
Matt Davis <maroda@gmail.com>
2020-08-21 17:41:36 -0700

    first working iteration (with known bugs)

--- README.md
+++ README.md
@@ -1,33 +1,16 @@
 # hpschd
 
+The Writing-Through Mesostic Generator
+
+A text file for input will be transmogrified into a piece of Mesostic poetry using a configured "Spine String".
+
 ## Mesostics
 
-The Main Text (MT) is simply any text input.
-
-The Spine String (SS) is all Capital Letters (CL).
+Of course not all mesostics are "writing through" style as Cage did often, they can just as easily be written as they are.
 
 - 50% Mesostic: The CL is unique between itself and the previous CL.
 - 100% Mesostic: The CL is unique between itself, the previous CL, and the next CL.
-
-A "meso-acrostic", arguably another version of a Mesostic, has neither of these limitations.
-
-The algorithm is:
-
-- SS is stored as an addressable array/slice of its characters CL
-- Each line of MT is fed into the function
-- If a character matches CL, logic happens
-- Some kind of line return function (LN) will need to exist to break lines, maybe a setting chopped by word.
-
-### Examples
-
-strings.Contains is probably important. for example with a 50% mesostic:
-
-0. lines LN are defined
-1. LN(l-?) is fed to the func
-2. function reads each character, knowing what the value of SS(CL-1) is, searching for the first occurance of SS(CL)
-3. when it finds the active SS(CL), strings.ToUpper
-4. that line is now done, loop moves on to the next SS(CL)
-
+- A "meso-acrostic", arguably another version of a Mesostic, has neither of these limitations.
 
 ## Display
 
--- mesostic.go
+++ mesostic.go
@@ -18,109 +18,79 @@ type LineFrag struct {
 	Data    string // The new Mesostic line.
 }
 
-// string slice for the collection of LineFrag entries to be sorted
+// LineFrags ::: string slice for the collection of LineFrag entries to be sorted
 type LineFrags []LineFrag
 
 var padCount int                          // global to track right-align padding
 var fragCount int                         // global to count total fragment combinations (i.e. lines)
 var fragMents = make(map[string]LineFrag) // Hash table of line fragments
+var ss string = "craque"                  // SpineString
+var sca []string                          // Characters in SpineString
+var ictus int                             // SpineString character address
 
 // mesoLine ::: finds the SpineString (SS) characters (sc)
-func mesoLine(s string, ss string, c int) {
+func mesoLine(s string, c int) {
 	var wstack []string // slice for rebuilding the west fragment
 	var estack []string // slice for rebuilding the east fragment
-	var sca []string    // Spine Characters in SpineString
 	var mode int
 
-	// step through each character of the string... is what we want to do here. but that is the tough part.
-	// get formatting fixed first with just a single character of the passed SpineString (ss)
-	for h := 0; h < len(ss); h++ {
-		sca = append(sca, string(ss[h]))
-	}
-
+CharLoop:
 	// step through the current string and process mesostic rules
 	for i := 0; i < len(s); i++ {
-		appC := string(s[i])
+		char := string(s[i])
 
 		switch mode {
 		// WestSide
 		case 0:
 			// as long as the character isn't the current Spine Character fill in the WestSide fragment.
-			if appC != sca[0] {
-				wstack = append(wstack, appC)
+			if char != sca[ictus] {
+				wstack = append(wstack, char)
 			} else {
 				// SpineString hit!
-				appC = strings.ToUpper(appC)  // Spine Character is capitalized
-				wstack = append(wstack, appC) // Appended to the string
+				char = strings.ToUpper(char)  // Spine Character is capitalized
+				wstack = append(wstack, char) // Appended to the string
 				mode = 1
-				break
+				break // re-evaluate the switch with mode set
 			}
 		// EastSide
 		case 1:
 			// The WestSide fragment is complete, fill in the remainder for the EastSide fragment.
-			//
-			// Make this configurable?
-			//
-			estack = append(estack, appC) // Full rest of line
-			// x := 0 + 1
-			//if appC != sca[x] {
-			//	estack = append(estack, appC)
-			//} else {
-			//	break
-			//}
+			if char != sca[ictus] {
+				estack = append(estack, char)
+			} else {
+				break CharLoop // We're done.
+			}
 		}
 	}
 
-	fragmentW := strings.Join(wstack, "")
-	fragmentE := strings.Join(estack, "")
+	// Post processing
+	Ictus(1)                                         // Increases the rotation of the SpineString
+	fragmentW := strings.Join(wstack, "")            // WestSide fragment
+	fragmentE := strings.Join(estack, "")            // EastSide fragment
 	fragCount++                                      // new line number of the current two fragments
 	fragkey := shakey(fragmentW + string(fragCount)) // unique identifier and consistent key sizes
+
+	/*
+
+		TODO: Lines without the current StringChar are being printed without that StringChar and the StringChar advances anyway
+
+	*/
 
 	// Some or all of this might be better off as pointers...
 	fragMents[fragkey] = LineFrag{Index: c, LineNum: fragCount, WChars: len(fragmentW), Data: fragmentW + fragmentE}
 
-	// the padding algo needs to be different. it will have to be the longest - present = padding
 	// record the longest WestSide fragment length
 	if len(fragmentW) > padCount {
 		padCount = len(fragmentW)
 	}
 }
 
-func main() {
-	// line counts for the Index
-	var lnc int
-
-	// process the given file, silent if no file given
-	for _, origtxt := range os.Args[1:] {
-		data, err := ioutil.ReadFile(origtxt)
-		if err != nil {
-			log.Fatal(err)
-			break
-		}
-		// run mesoLine - which needs a new name - to process each line
-		for _, line := range strings.Split(string(data), "\n") {
-			lnc++
-			mesoLine(strings.ToLower(line), "mat", lnc)
-		}
-	}
-
-	// Sort & Print //
-	//
-	// Lines to be sorted are pushed to a slice.
-	// Sort is configured on LineNum.
-	// Uneven padding is accomplished by subtracting the length of the current WestSide fragment
-	// from the length of the longest WestSide fragment (padCount)
-	var linefragments LineFrags
-	for k, _ := range fragMents {
-		linefragments = append(linefragments, fragMents[k])
-	}
-
-	sort.Sort(linefragments)
-
-	for i := 0; i < len(linefragments); i++ {
-		padMe := padCount - linefragments[i].WChars
-		spaces := strings.Repeat(" ", padMe)
-		fmt.Printf("%s%s\n", spaces, linefragments[i].Data)
+// Ictus :: Rotates SpineString characters
+func Ictus(i int) {
+	if ictus < len(ss)-1 {
+		ictus += i
+	} else if ictus == len(ss)-1 {
+		ictus = 0
 	}
 }
 
@@ -142,4 +112,49 @@ func shakey(k string) string {
 	bash := s.Sum(nil)
 	hash := fmt.Sprintf("%x", bash)
 	return hash
+}
+
+func main() {
+	// line counts for the Index
+	var lnc int
+
+	for h := 0; h < len(ss); h++ {
+		sca = append(sca, string(ss[h]))
+	}
+
+	// process the given file, silent if no file given
+	for _, origtxt := range os.Args[1:] {
+		data, err := ioutil.ReadFile(origtxt)
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+
+		// run mesoLine - which needs a new name - to process each line
+		for _, line := range strings.Split(string(data), "\n") {
+			lnc++
+			mesoLine(strings.ToLower(line), lnc)
+		}
+	}
+
+	// Sort & Print //
+	//
+	// Lines to be sorted are pushed to a slice.
+	// Sort is configured on LineNum.
+	// Uneven padding is accomplished by subtracting
+	//   the length of the current WestSide fragment
+	//   from the length of the longest WestSide fragment (padCount)
+
+	var linefragments LineFrags
+	for k := range fragMents {
+		linefragments = append(linefragments, fragMents[k])
+	}
+
+	sort.Sort(linefragments)
+
+	for i := 0; i < len(linefragments); i++ {
+		padMe := padCount - linefragments[i].WChars
+		spaces := strings.Repeat(" ", padMe)
+		fmt.Printf("%s%s\n", spaces, linefragments[i].Data)
+	}
 }
