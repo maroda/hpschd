41311c4fb9664eb8132fa4507bf33b3b90c764cd
Matt Davis <maroda@gmail.com>
2020-10-05 12:51:56 -0700

    removing ops materials

--- tf/route53/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "zoneapex" {
-  default = "hpschd.xyz"
-}
-
-variable "ecs" {
-  description = "The ECS ALB endpoint for DNS CNAME"
-  default     = "hpschd-mesostic-api-1116213348.us-west-2.elb.amazonaws.com"
-}
